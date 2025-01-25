f7c07298176929a64f26f38fb8f4b7a128ea2486
Dheeraj.mann@hotmail <Dheeraj.mann@hotmail.com>
2025-01-25 11:27:25 +0530

    First commmit

---
+++ cypress/e2e/loginWithEmail.cy.js
@@ -0,0 +1,26 @@
+describe('Login scenerios', () => {
+  it('User can login with correct email and password', () => {
+    cy.intercept('POST', '**/events/bulk/*').as('waitForEmailorPasswordPopUpToAppear');
+    cy.intercept('POST', " https://www.google-analytics.com/g/**").as('waitForHomePageToLoad');
+
+    cy.visit('https://www.roon.com/')
+    cy.get('[aria-label="Press to open modal"]').click();
+    cy.contains('Log In').click();
+    cy.contains("Continue with Email").click();
+
+    cy.wait('@waitForEmailorPasswordPopUpToAppear');
+    cy.get('input[type="email"]').type('PASTE_YOUR_EMAIL_HERE');
+    cy.contains("Continue").click();
+
+    cy.wait('@waitForEmailorPasswordPopUpToAppear');
+    cy.get('input[type="password"]').type('PASTE_YOUR_PASSWORD_HERE');
+    cy.contains("Continue").click();
+
+    cy.wait('@waitForHomePageToLoad');
+
+    cy.contains('Profile').click({force: true});
+    cy.get('.ml-auto').click();
+    cy.contains('Logout').click();
+
+  })
+})
